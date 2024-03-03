1df69b81e0b7d78d419d1e5bd8ca26db96dc69a8
Shoaib Kalawant <skalawan@stevens.edu>
2024-03-02 19:09:46 -0500

    Initial folder Structure and React Frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import PatientInput from "./components/PatientInput";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pinput" element={<PatientInput />} />
+        </Routes>
+      </main>
+      <footer></footer>
+    </Router>
+  );
+}
+
+export default App;

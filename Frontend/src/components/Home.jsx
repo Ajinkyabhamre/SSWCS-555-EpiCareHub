d561182f6626823a7763ab0b5e925eee0d88b9b2
Ajju <abhamre50@gmail.com>
2024-03-03 21:28:25 -0500

    home page done

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -3,12 +3,16 @@ import BrainImage from '../assets/brain.png'; // Importing the image
 
 const Home = () => {
   return (
-    <div>
+    <div className="container"> 
+   <div className="row content">
+
       <h1>Welcome to the Presurgical Epilepsy Evaluation Platform!</h1>
       <p>This web application platform is designed to assist doctors in accurately locating epileptic seizure areas in epilepsy patients' brains.</p>
       <p>By utilizing advanced 3D visualization techniques, doctors can interact with reconstructed brain models, enabling them to pinpoint areas of abnormal brain activity associated with epilepsy.</p>
-      <p>This visualization provides crucial reference points for pre-surgical assessment, aiding doctors in planning and executing surgeries with greater precision and effectiveness.</p>
-      <img src={BrainImage} alt="3D Brain Image" style={{ width: '100%', maxWidth: '500px', margin: '20px auto' }} />
+            <p>This visualization provides crucial reference points for pre-surgical assessment, aiding doctors in planning and executing surgeries with greater precision and effectiveness.</p>
+        <img src={BrainImage} alt="3D Brain Image" className="img-fluid" />
+          
+  </div>
     </div>
   );
 };

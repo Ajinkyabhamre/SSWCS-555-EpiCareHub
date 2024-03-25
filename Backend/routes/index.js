847701a92197f42521aebf3c457d841dc4310c25
pyadav6 <pyadav6@stevens.edu>
2024-03-24 23:47:52 -0400

    User data fetching API
    User data fetching API

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,11 @@
 import paitentRoutes from "./patients.js";
 import userRoutes from "./user.js";
+import usersDetailRoutes from "./usersDetails.js";
 
 const constructorMethod = (app) => {
   app.use("/patients", paitentRoutes);
   app.use("/users", userRoutes);
+  app.use("/usersDetails", usersDetailRoutes);
 
   app.use("*", (req, res) => {
     return res.status(404).json({ error: "Not found" });

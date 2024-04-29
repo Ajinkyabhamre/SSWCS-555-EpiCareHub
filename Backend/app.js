1022044e7d28c3c6c122765977f13c25998c7e31
ahujagautam024 <ahujagautam024@gmail.com>
2024-04-29 17:43:55 -0400

    login data functions done

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,20 @@ import cors from "cors";
 const app = express();
 import configRoutesFunction from "./routes/index.js";
 
+import session from "express-session";
+
 import fileUpload from "express-fileupload";
 
 import bodyParser from "body-parser";
+
+app.use(
+  session({
+    name: "AuthenticationSession",
+    secret: "Epicare Hub data message",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
@@ -18,4 +29,4 @@ app.listen(3000, () => {
   console.log("Your routes will be running on http://localhost:3000");
 });
 
-export default app; 
+export default app;
